186f6eafdf7225e8bd591e6656d3fbb275ebd3fa
joshparkerj <superfakek@gmail.com>
2019-05-07 21:49:03 -0500

    change api path to api

--- src/main/java/com/revature/rest/HelloApplication.java
+++ src/main/java/com/revature/rest/HelloApplication.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/v1")
+@ApplicationPath("/api")
 public class HelloApplication extends Application{
 	@Override
 	public Set< Class<?> > getClasses() {

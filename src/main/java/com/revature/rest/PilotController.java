d3f0042120bc26464da994868c5baffc62945037
joshparkerj <superfakek@gmail.com>
2019-05-09 14:25:57 -0500

    add schema for each resource

--- src/main/java/com/revature/rest/PilotController.java
+++ src/main/java/com/revature/rest/PilotController.java
@@ -27,8 +27,9 @@ public class PilotController {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getPilots(@DefaultValue("1") @QueryParam("page") int page, @DefaultValue("") @QueryParam("search") String searchparam) {
-		List<Pilot> p = Access.getAccess().Pilot.getPilots(page,searchparam);
+	public String getPilots(@DefaultValue("1") @QueryParam("page") int page,
+			@DefaultValue("") @QueryParam("search") String searchparam) {
+		List<Pilot> p = Access.getAccess().Pilot.getPilots(page, searchparam);
 		int pilotCount = Access.getAccess().Pilot.getCount();
 		StringBuilder sb = new StringBuilder();
 		sb.append("{\"count\":");
@@ -69,6 +70,13 @@ public class PilotController {
 			e.printStackTrace();
 			return "{}";
 		}
+	}
+
+	@GET
+	@Path("/schema/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getPilotSchema() {
+		return "{ \"title\": \"Pilot Object\", \"$schema\": \"http://json-schema.org/draft-04/schema\", \"description\": \"A pilot who has graduated from Flight School.\", \"type\": \"object\", \"required\": [  \"id\",  \"name\",  \"dob\",  \"sex\",  \"url\",  \"age\",  \"aircraft_count\" ], \"properties\": {  \"id\": {   \"description\": \"The unique integer identifier for this pilot.\",   \"type\": \"integer\"  },  \"name\": {   \"description\": \"The name of this pilot.\",   \"type\": \"string\"  },  \"dob\": {   \"format\": \"date\",   \"description\": \"The pilot's date of birth in YYYY-MM-DD format.\",   \"type\": \"string\"  },  \"sex\": {   \"description\": \"The pilot's sex.\",   \"type\": \"string\"  },  \"url\": {   \"description\": \"The hypermedia URL of this resource.\",   \"type\": \"string\"  },  \"age\": {   \"description\": \"The pilot's age in years.\",   \"type\": \"integer\"  },  \"aircraft_count\": {   \"description\": \"The number of aircraft this pilot has been trained and certified to fly.\",   \"type\": \"integer\"  },  \"craft\": {   \"description\": \"An array of the aircraft this pilot has been trained and certified to fly.\",   \"type\": \"array\",   \"items\": {    \"type\": \"object\",    \"required\": [     \"id\",     \"name\",     \"certs\",     \"pilot_count\",     \"url\"    ],    \"properties\": {     \"id\": {      \"description\": \"The unique integer that identifies the aircraft.\",      \"type\": \"integer\"     },     \"name\": {      \"description\": \"The name of the aircraft.\",      \"type\": \"string\"     },     \"certs\": {      \"description\": \"A comma-separated list of the certifications that allow a pilot to fly this aircraft.\",      \"type\": \"string\"     },     \"pilot_count\": {      \"description\": \"The number of pilots from Flight School who are certified to fly this aircraft.\",      \"type\": \"integer\"     },     \"url\": {      \"description\": \"The hypermedia URL for the aircraft.\",      \"type\": \"string\"     }    }   }  } }}";
 	}
 
 }
